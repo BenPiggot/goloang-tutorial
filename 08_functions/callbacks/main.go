0ea4cbd4eca83057b5ec8b70b856919b6106dfb2
ben <wbpiggot@gmail.com>
2017-09-15 17:17:21 -0700

    Fixing callbacks file

--- 08_functions/callbacks/main.go
+++ 08_functions/callbacks/main.go
@@ -8,6 +8,26 @@ func forEach(numbers []int, callback func(int)) {
   }
 }
 
+func filter(numbers []int, callback func(int) bool) []int {
+  filtered := []int{}
+  for _, n := range numbers {
+    if callback(n) {
+      filtered = append(filtered, n)
+    }
+  }
+
+  return filtered
+}
+
+func mapIt(numbers []int, callback func(int) int) []int {
+  mapped := []int{}
+  for _, n := range numbers {
+    mapped = append(mapped, callback(n))
+  }
+
+  return mapped
+}
+
 func main() {
   forEach([]int{1,2,3,4}, func(x int) {
     fmt.Println(x)
